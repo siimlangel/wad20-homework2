780c25cb07643b45336a01ddf603a8c304c5656b
martentyrk <martentyrk@gmail.com>
2020-10-19 16:48:44 +0300

    Task 2 finished

--- res/scripts/task2.js
+++ res/scripts/task2.js
@@ -1,25 +1,86 @@
-const posts = []
+let posts = [];
 
 $(function () {
-    loadPosts().then(function (response) {
-        for(let post of response) {
-            console.log(post.author.firstname);
-        }
-    })
+  loadPosts().then(function (response) {
+    for (let post of response) {
+      const authorName = post.author.firstname + " " + post.author.lastname;
 
-
+      posts.push(
+        new Post(
+          authorName,
+          post.author.avatar,
+          post.createTime,
+          //Checking post.text value
+          post.text ? post.text : "",
+          //Checking media.type not null.
+          post.media ? (post.media.type ? post.media.type : "") : "",
+          //Checking post.media.url that it's not null.
+          post.media ? (post.media.url ? post.media.url : "") : "",
+          post.likes
+        )
+      );
+    }
+    displayPosts(posts);
+  });
 });
 
-function loadPosts() {
-    return $.get(
-        {
-            url: 'https://private-anon-87bbb94209-wad20postit.apiary-mock.com/posts',
-            success: function (response) {
-                return response;
-            },
-            error: function () {
-                alert('Error with reading posts.')
-            }
-        }
+function displayPosts(posts) {
+  for (let post of posts) {
+    let postDiv    = $("<div />").addClass("post");
+
+    //Post author information
+    let authorDiv  = $("<div />").addClass("post-author");
+    let authorSpan = $("<span />").addClass("post-author-info");
+
+    authorSpan.append(`<img id="image" src=${post.avatar}>`);
+    authorSpan.append(`<small>${post.author}</small>`);
+    //Adding to the authorDiv in the right order.
+    authorDiv.append(authorSpan);
+    authorDiv.append(`<small>${post.createTime}</small>`);
+
+    //Post image information
+    let postImageDiv = $("<div />").addClass("post-image");
+
+    if (post.mediaType === "image") {
+      postImageDiv.append(`<img alt="Post Author" src=${post.mediaURL}>`);
+    } else if (post.mediaType === "video") {
+      var video = $("<video />", {
+        id: "video",
+        src: post.mediaURL,
+        type: "video/mp4",
+        controls: true,
+      });
+      postImageDiv.append(video);
+    }
+
+    //Post text information
+    let postTextDiv = $("<div />").addClass("post-title");
+    postTextDiv.append(`<h3>${post.text}</h3>`);
+
+    //Post likes information
+    let postActionsDiv = $("<div />").addClass("post-actions");
+    postActionsDiv.append(
+      `<button type="button" name="like" class="like-button">${post.likes}</button>`
     );
+    
+    //Adding all the created divs to the main postDiv.
+    postDiv.append(authorDiv);
+    postDiv.append(postImageDiv);
+    postDiv.append(postTextDiv);
+    postDiv.append(postActionsDiv);
+
+    $("section.main-container").append(postDiv);
+  }
+}
+
+function loadPosts() {
+  return $.get({
+    url: "https://private-anon-87bbb94209-wad20postit.apiary-mock.com/posts",
+    success: function (response) {
+      return response;
+    },
+    error: function () {
+      alert("Error with reading posts.");
+    },
+  });
 }
